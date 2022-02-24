8de56d20221eb8e62127aac16985aaadca8a75ca
Bochia <ochia@wisc.edu>
2022-02-24 18:03:14 +0100

    Added a continue/cance buton.

--- CopyPasteBind.py
+++ CopyPasteBind.py
@@ -1,6 +1,8 @@
 from time import sleep
 import keyboard
 import pyperclip
+import easygui
+import sys
 
 def SaveCopiedTextToList(copy_bind_values, key):
     keyboard.press_and_release("ctrl+c")
@@ -14,7 +16,6 @@ def PasteValue(copy_bind_pairs, key):
 def CreateHotKey(copy_bind_pairs, numberKey):
     keyboard.add_hotkey(f"ctrl+c+{numberKey}", SaveCopiedTextToList, args=(copy_bind_pairs, numberKey))
     keyboard.add_hotkey(f"ctrl+b+{numberKey}", PasteValue, args=(copy_bind_pairs, numberKey))
-
 
 def CreateHotKeys():
 
@@ -34,14 +35,20 @@ def CreateHotKeys():
     for numberKey in range(0,9):
         CreateHotKey(copy_bind_pairs, numberKey)
 
-
-
-
 #Main
 CreateHotKeys()
 
-while True:  
+appOpen = True
+
+while appOpen:  
     try:
-        sleep(2)
+        msg = "Do you want to continue using the copy bind hotkeys?"
+        title = "Please Confirm"
+        if easygui.ccbox(msg, title):  # show a Continue/Cancel dialog
+            appOpen = True 
+        else:  
+            # user chose Cancel
+            appOpen = False
+            sys.exit(0)
     except:
         break
