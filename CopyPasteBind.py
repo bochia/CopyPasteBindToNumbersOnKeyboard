9224b51591cf510b206011a13eb8d5115b755346
Bochia <brandon.ochia@gmail.com>
2022-07-30 16:03:55 +0200

    Did some refactoring on how pages are shown and hidden.

--- CopyPasteBind.py
+++ CopyPasteBind.py
@@ -37,27 +37,36 @@ def CreateHotKeys():
     for numberKey in range(0,9):
         CreateHotKey(copy_bind_pairs, numberKey)  
 
+def hide_page(page):
+    page.setAttribute(CSS_STYLE_ATTRIBUTE, CSS_DISPLAY_NONE)
+
 def openPage_configuration():
-    mainPage.setAttribute(CSS_STYLE_ATTRIBUTE, CSS_DISPLAY_NONE)
-    howToUsePage.setAttribute(CSS_STYLE_ATTRIBUTE, CSS_DISPLAY_NONE)
-    configurationPage.setAttribute(CSS_STYLE_ATTRIBUTE, CSS_DISPLAY_BLOCK)
+    show_page_and_hide_all_others(CONFIGURATION_PAGE_ID)
     print("Made it to openPage_configuration.")
 
 def openPage_how_to_use():
-    mainPage.setAttribute(CSS_STYLE_ATTRIBUTE, CSS_DISPLAY_NONE)
-    howToUsePage.setAttribute(CSS_STYLE_ATTRIBUTE, CSS_DISPLAY_BLOCK)
-    configurationPage.setAttribute(CSS_STYLE_ATTRIBUTE, CSS_DISPLAY_NONE)
+    show_page_and_hide_all_others(HOW_TO_USE_PAGE_ID)
     print("Made it to openPage_how_to_use")
 
 def openPage_main():
-    mainPage.setAttribute(CSS_STYLE_ATTRIBUTE, CSS_DISPLAY_BLOCK)
-    howToUsePage.setAttribute(CSS_STYLE_ATTRIBUTE, CSS_DISPLAY_NONE)
-    configurationPage.setAttribute(CSS_STYLE_ATTRIBUTE, CSS_DISPLAY_NONE)
+    show_page_and_hide_all_others(MAIN_PAGE_ID)
     print("Made it to openPage_main.")
 
 def PasteBindedValue(copy_bind_pairs, numberKey):
     pyperclip.copy(copy_bind_pairs[numberKey])
     keyboard.press_and_release("ctrl+v")    
+
+def show_page(page):
+    page.setAttribute(CSS_STYLE_ATTRIBUTE, CSS_DISPLAY_BLOCK)
+
+def show_page_and_hide_all_others(page_Id):
+    show_page(win.getElementById(page_Id))
+    
+    page_Ids_to_hide = [x for x in all_page_Ids if x != page_Id]
+
+    for page_Id_to_hide in page_Ids_to_hide:
+        page = win.getElementById(page_Id_to_hide)
+        hide_page(page)
 
 ########### Main Program ########### 
 CreateHotKeys()
@@ -74,6 +83,8 @@ try:
     mainPage = win.getElementById(MAIN_PAGE_ID)
     howToUsePage = win.getElementById(HOW_TO_USE_PAGE_ID)
     configurationPage = win.getElementById(CONFIGURATION_PAGE_ID)
+
+    all_page_Ids = [MAIN_PAGE_ID, HOW_TO_USE_PAGE_ID, CONFIGURATION_PAGE_ID]
     
     win.show()
 
