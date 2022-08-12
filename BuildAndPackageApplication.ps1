db1ad97e259619af9fae12a428a3311eb6c44b27
Bochia <brandon.ochia@gmail.com>
2022-08-12 20:27:49 +0200

    Built executable and udpate powershell script to handle new settings file.

--- BuildAndPackageApplication.ps1
+++ BuildAndPackageApplication.ps1
@@ -1,12 +1,18 @@
 $currentDirectory = Get-Location
 
-$cssFolderPath = Join-Path -Path $currentDirectory -ChildPath "css"
-$htmlFolderPath = Join-Path -Path $currentDirectory -ChildPath "html"
+$cssString = "css"
+$htmlString = "html"
+$settingsString = "settings"
+
 $distFolderPath = Join-Path -Path $currentDirectory -ChildPath "dist"
+$cssFolderPath = Join-Path -Path $currentDirectory -ChildPath $cssString
+$htmlFolderPath = Join-Path -Path $currentDirectory -ChildPath $htmlString
+$settingsFolderPath = Join-Path -Path $currentDirectory -ChildPath $settingsString
 
 # Delete old folders from previous build.
-$cssDistFolderPath = Join-Path -Path $distFolderPath -ChildPath "css"
-$htmlDistFolderPath = Join-Path -Path $distFolderPath -ChildPath "html"
+$cssDistFolderPath = Join-Path -Path $distFolderPath -ChildPath $cssString
+$htmlDistFolderPath = Join-Path -Path $distFolderPath -ChildPath $htmlString
+$settingsDistFolderPath = Join-Path -Path $distFolderPath -ChildPath $settingsString
 if (Test-Path -Path $cssDistFolderPath) {
     Remove-Item $cssDistFolderPath -Recurse
 }
@@ -15,8 +21,13 @@ if (Test-Path -Path $htmlDistFolderPath) {
     Remove-Item $htmlDistFolderPath -Recurse
 }
 
+if (Test-Path -Path $settingsDistFolderPath) {
+    Remove-Item $settingsDistFolderPath -Recurse
+}
+
 # Copy new files into dist folder.
 Copy-Item -Path $cssFolderPath -Destination $distFolderPath -Recurse
 Copy-Item -Path $htmlFolderPath -Destination $distFolderPath -Recurse
+Copy-Item -Path $settingsFolderPath -Destination $distFolderPath -Recurse
 
 pyinstaller --onefile --noconsole CopyPasteBind.py --add-data="html\MainWindow.html;." --add-data="css\style.css;."
